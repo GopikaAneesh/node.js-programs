96cb87cda8108da2819c7c9da45dd53233a243c7
GopikaAneesh <102350413+GopikaAneesh@users.noreply.github.com>
2022-11-06 19:39:06 +0400

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,18 @@
+
+
+const fs = require('fs');
+const csv = require('csv-parser');
+const results= [];
+const readCSVFile = async () => {
+ fs.createReadStream('data.csv')
+.pipe(csv())
+.on('data',(data)=>results.push(data))
+.on('end', () => {
+      
+      const found = results.find(({ LastName }) => LastName === "Alberto");
+      
+      console.log(found); 
+    
+  });
+};
+ readCSVFile()
